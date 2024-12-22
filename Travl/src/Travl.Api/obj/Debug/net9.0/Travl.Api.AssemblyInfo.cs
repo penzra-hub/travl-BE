f3acf1b52f07a94bce6de285e162c0fd823b24e6
CalebTek <olawale.odeyemi@decagon.dev>
2024-12-22 11:01:40 +0100

    Added Travl Domain Project

--- Travl/src/Travl.Api/obj/Debug/net9.0/Travl.Api.AssemblyInfo.cs
+++ Travl/src/Travl.Api/obj/Debug/net9.0/Travl.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Travl.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7660aa7aa2e0351be18e1bac89bdc9edcc689a56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05875ce978c5afd3af5e9a6a7dce3e9ecef511b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Travl.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Travl.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
