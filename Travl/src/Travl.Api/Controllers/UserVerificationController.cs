3343966d96bbd70c7efd410c6e052560da930cea
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2025-03-05 09:09:18 +0100

    Implement driver kyc submission endpoint

---
+++ Travl/src/Travl.Api/Controllers/UserVerificationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Travl.Application.Drivers.Commands;
+
+namespace Travl.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserVerificationController : ApiController
+    {
+        [HttpPost("driver-kyc/submit")]
+        public async Task<IActionResult> SubmitVerification([FromForm] SubmitDriverVerificationCommand command)
+        {
+            return await Initiate(() => Mediator.Send(command));
+        }
+    }
+}
